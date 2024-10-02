b1657814aeb138f2fd791b0be4fccfb2c28a246b
Maria.r.williams@accolade.com <Maria.r.williams@accolade.com>
2024-10-02 14:12:55 +0000

    start matching notebook

---
+++ cohort_mapping/main_matching.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC - DataPrep
+# MAGIC   - list available event categories
+# MAGIC   - choose 'exposed' cohort
+# MAGIC   - choose 'control' cohort
+# MAGIC   - collect matching variables for both
+# MAGIC - Cohort Matching
+# MAGIC   - scale, etc. to prep for matching
+# MAGIC   - run matching algorithm
+# MAGIC   - export matching data
