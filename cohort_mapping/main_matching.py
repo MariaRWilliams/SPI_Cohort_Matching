30e850bdcc55de796b4c5746857b51f70e068642
Maria.r.williams@accolade.com <Maria.r.williams@accolade.com>
2024-10-03 17:49:11 +0000

    starting data prep

--- cohort_mapping/main_matching.py
+++ cohort_mapping/main_matching.py
@@ -20,7 +20,7 @@ pc = prep_class.Data_Prep()
 
 # COMMAND ----------
 
-#these two lines bring in the code again if updated after original run
+#these lines bring in the code again if updated after original run
 import importlib
 import src.prep_class as prep_class
 importlib.reload(prep_class)
@@ -36,13 +36,15 @@ event_df = (
 )
 
 event_df = event_df.toPandas()
+event_df['utc_period'] = pd.to_datetime(event_df['utc_period'].astype(str), format='%Y%m')
 pc.set_categories(event_df)
+
 print(pc.category_list)
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ##Select Exposed Subset
+# MAGIC ###Select Exposed Subset
 
 # COMMAND ----------
 
@@ -51,8 +53,90 @@ print(pc.category_list)
 preperiod = 3
 postperiod = 0
 
-exposed_subset = pc.clean_exposed(event_df, preperiod, postperiod)
+exposed_subset = event_df[event_df['category']!='exclude'].copy()
+exposed_subset = pc.clean_exposed(exposed_subset, preperiod, postperiod)
 
 # COMMAND ----------
 
-print(exposed_subset.head())
+#print(exposed_subset.head()
+print('Customers: '+ str(len(exposed_subset['org_nm'].unique().tolist())))
+print('Event sample size:')
+print(exposed_subset[['category', 'person_id']].groupby('category').count().reset_index())
+
+# COMMAND ----------
+
+print(exposed_subset.columns)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###Select Control Subset - from Accolade Members
+
+# COMMAND ----------
+
+#get member data
+#note: if get deletionVectors error, update cluster to one with Databricks Runtime 12.2 LTS - 15.3
+mem_df = (
+    spark
+    .sql("SELECT * FROM dev.`clinical-analysis`.cohort_matching_cg_mem")
+)
+
+mem_df = mem_df.toPandas()
+mem_df['start_date'] = pd.to_datetime(mem_df['start_date'])
+mem_df['end_date'] = pd.to_datetime(mem_df['end_date'])
+print(mem_df.head())
+
+
+# COMMAND ----------
+
+#remove members with events
+control_subset = mem_df[~mem_df['person_id'].isin(event_df['person_id'])]
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###Select Control Subset - from Marketscan Members
+
+# COMMAND ----------
+
+#do this once Marketscan is available
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###Add features and prepare for matching
+
+# COMMAND ----------
+
+#pull in claims and utilization data
+claims_df = (
+    spark
+    .sql("SELECT * FROM dev.`clinical-analysis`.cohort_matching_cg_claims")
+)
+
+claims_df = claims_df.toPandas()
+claims_df['service_month'] = pd.to_datetime(claims_df['service_month'].astype(str), format='%Y%m')
+print(claims_df.head())
+
+# COMMAND ----------
+
+#add claims to member demographics - this also removes members without claims
+exposed_subset = mem_df.merge(exposed_subset, on='person_id', how = 'inner')
+exposed_subset = exposed_subset[(exposed_subset['utc_period']>=exposed_subset['start_date']) &
+                                (exposed_subset['utc_period']<=exposed_subset['end_date'])]
+
+exposed_subset = exposed_subset.merge(claims_df, how='inner', on=['dw_member_id'])
+control_subset = control_subset.merge(claims_df, how='inner', on=['dw_member_id'])
+
+exposed_subset = exposed_subset[(exposed_subset['service_month']>=exposed_subset['start_date']) &
+                  (exposed_subset['service_month']<=exposed_subset['end_date'])]
+control_subset = control_subset[(control_subset['service_month']>=control_subset['start_date']) &
+                  (control_subset['service_month']<=control_subset['end_date'])]
+
+# COMMAND ----------
+
+exposed_subset_2.head()
+
+# COMMAND ----------
+
+#pivot claims and utilization
--- cohort_mapping/src/prep_class.py
+++ cohort_mapping/src/prep_class.py
@@ -3,31 +3,15 @@ import pandas as pd
 class Data_Prep():
 
     def __init__(self):
-        category_list = []
+        self.category_list = []
 
     def set_categories(self, event_df):
 
         self.category_list = event_df['category'].unique()
 
         return 0
-    
-    def interventions_subset(self, m, big_intervention_df, pre_event, post_event, intervention_month):
-                            
-        i_month_label = intervention_month
-        intervention_month = m
-     
-        prior_start = self.month_shift(intervention_month,-pre_event)
-        last_month = self.month_shift(intervention_month,post_event)
-        
-        SPI_df = big_intervention_df[(big_intervention_df['utc_period']>=prior_start) & (big_intervention_df['utc_period']<=last_month)].copy()
-        SPI_df['int_month'] = i_month_label
-        SPI_df['int_count'] = 1 
-        
-        return SPI_df
-    
+       
     def clean_exposed(self, event_df, preperiod, postperiod):
-
-        event_df['utc_period'] = pd.to_datetime(event_df['utc_period'].astype(str), format='%Y%m')
 
         exposed_df = pd.DataFrame()
         for category in self.category_list:
@@ -43,7 +27,7 @@ class Data_Prep():
                             (cat_df['utc_period_y'].isna())]
 
             cat_df = cat_df[['org_nm_x', 'person_id', 'drvd_mbrshp_covrg_id_x', 'utc_period_x', 'category_x']]
-            cat_df = cat_df.rename(columns={'org_nm_x':'org_nm','drvd_mbrshp_covrg_id_x':'drvd_mbrshp_covrg_id', 'utc_period_x':'event_period', 'category_x':'category'})
+            cat_df = cat_df.rename(columns={'org_nm_x':'org_nm','drvd_mbrshp_covrg_id_x':'drvd_mbrshp_covrg_id', 'utc_period_x':'utc_period', 'category_x':'category'})
             exposed_df = pd.concat([exposed_df, cat_df])
 
         return exposed_df
