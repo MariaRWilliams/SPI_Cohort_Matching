87cc2d4fb5dc44ae3720cabdcd380cde789e754b
Maria.r.williams@accolade.com <Maria.r.williams@accolade.com>
2024-11-20 13:27:29 +0000

    data exploration

--- cohort_mapping/3_main_process_cohorts.py
+++ cohort_mapping/3_main_process_cohorts.py
@@ -14,6 +14,7 @@ pc = prep_class.Data_Prep()
 
 # COMMAND ----------
 
+#matched_df has matching variables, details_df has additional information
 matched_df = pc.query_data(spark, dbutils, 'cohort_matching_cohorts_matched')
 details_df = pc.query_data(spark, dbutils, 'cohort_matching_cohorts')
 
@@ -39,24 +40,19 @@ col = display_id_col + [column for column in details_df.columns if column.starts
 
 # COMMAND ----------
 
-#join comparison details to matched cohort
-matched_df = matched_df.withColumn('category_long', matched_df['category'])
-matched_df = matched_df.withColumn('category', F.when(F.col('category').contains('control'), 'control').otherwise(F.col('category')))
-
-full_df = matched_df.alias('df1').join(details_df.alias('df2'), join_id_col, 'left').select('df2.*', 'category_long')
-full_df = full_df.withColumn('category', full_df['category_long'])
-
-# COMMAND ----------
-
-#stats
-full_df.groupby('category').agg(F.count('person_id').alias('count'), 
+#stats - will change when joined to other details 
+#would be great to get what matched on exactly, and not exactly, but that would need to be exported at earlier step
+#how many matches made, how close they are on average
+agg_df = matched_df.groupby('category').agg(F.count('person_id').alias('count'), 
                                 F.round(F.sum('cancer')).alias('members with cancer'), 
                                 F.round(F.sum('diabetes')).alias('members with diabetes'), 
                                 F.round(F.mean('total_allowed-1'), 2).alias('avg spend at period -1'), 
                                 F.round(F.mean('age'), 2).alias('avg age'),
                                 F.min('utc_period').alias('min interaction period'),
                                 F.max('utc_period').alias('max interaction period')
-                                ).display()
+                                )
+
+agg_df.display()
 
 # COMMAND ----------
 
@@ -65,11 +61,20 @@ full_df.groupby('category').agg(F.count('person_id').alias('count'),
 
 # COMMAND ----------
 
-agg_df = full_df.select(*col)
-agg_df = agg_df.groupby(*display_id_col).agg(*[F.round(F.avg(F.col(x)),2).alias(x) for x in col if x not in join_id_col])
-agg_df.display()
+#join comparison details to matched cohort
+matched_df = matched_df.withColumn('category_long', matched_df['category'])
+matched_df = matched_df.withColumn('category', F.when(F.col('category').contains('control'), 'control').otherwise(F.col('category')))
 
-chart_df = agg_df.toPandas()
+full_df = matched_df.alias('df1').join(details_df.alias('df2'), join_id_col, 'left').select('df2.*', 'category_long')
+full_df = full_df.withColumn('category', full_df['category_long']).distinct()
+
+# COMMAND ----------
+
+chart_df = full_df.select(*col)
+chart_df = chart_df.groupby(*display_id_col).agg(*[F.round(F.avg(F.col(x)),2).alias(x) for x in col if x not in join_id_col])
+chart_df.display()
+
+chart_df = chart_df.toPandas()
 chart_df = chart_df[col].set_index('category').T
 #print(chart_df)
 chart_df.plot.line(figsize = (15,5), title = compare_col_prefix[0]).vlines(x=3, ymin=0, ymax=chart_df.to_numpy().max(), ls='--')
